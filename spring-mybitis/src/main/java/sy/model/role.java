cb97867051e3d948a39dbdffd1929e702e9e2be7
ZuoYouLai <ZuoYouLai@foxmail.com>
2015-04-29 00:51:15 +0800

    3
    ererr

---
+++ spring-mybitis/src/main/java/sy/model/role.java
@@ -0,0 +1,33 @@
+package sy.model;
+
+public class role {
+    private String id;
+
+    private String rname;
+
+    private String rdesc;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id == null ? null : id.trim();
+    }
+
+    public String getRname() {
+        return rname;
+    }
+
+    public void setRname(String rname) {
+        this.rname = rname == null ? null : rname.trim();
+    }
+
+    public String getRdesc() {
+        return rdesc;
+    }
+
+    public void setRdesc(String rdesc) {
+        this.rdesc = rdesc == null ? null : rdesc.trim();
+    }
+}
---
+++ spring-mybitis/src/main/java/sy/model/roleuserKey.java
@@ -0,0 +1,23 @@
+package sy.model;
+
+public class roleuserKey {
+    private String uid;
+
+    private String rid;
+
+    public String getUid() {
+        return uid;
+    }
+
+    public void setUid(String uid) {
+        this.uid = uid == null ? null : uid.trim();
+    }
+
+    public String getRid() {
+        return rid;
+    }
+
+    public void setRid(String rid) {
+        this.rid = rid == null ? null : rid.trim();
+    }
+}
